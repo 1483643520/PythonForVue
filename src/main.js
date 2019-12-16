7b443d8781ffffd55946632287afd5d43bac851b
1483643520 <1483643520@qq.com>
2019-12-16 17:46:29 +0800

    ElementUi的调用及安装 一

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 //导入vue js
 import Vue from 'vue'
+//导入element-ui相关
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 //导入根组件
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+//使用elementui
+Vue.use(ElementUI);
 
 //创建实例
 new Vue({
-  render: h => h(App),  //渲染App根组件
-}).$mount('#app')  //指定div 的 id
+    render: h => h(App),  //渲染App根组件
+}).$mount('#app'); //指定div 的 id
